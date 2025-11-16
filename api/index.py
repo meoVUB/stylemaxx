af9f326f5731749abc9c098523284ad3561e111b
Mehmet Serdar Uz <112027633+meoVUB@users.noreply.github.com>
2025-11-16 11:35:50 +0100

    fix vercel bug

--- api/index.py
+++ api/index.py
@@ -1,3 +1,12 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(root_dir)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stylemaxx.settings")
+
 from django.core.wsgi import get_wsgi_application
 
 app = get_wsgi_application()
